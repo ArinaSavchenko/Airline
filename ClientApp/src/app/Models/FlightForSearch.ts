d816db29df18466c4104645525c8b1efa80e138b
Aryna Sauchanka <arinasavchenko2302@gmail.com>
2020-10-05 12:04:19 +0300

    Corrected mistakes

--- ClientApp/src/app/Models/FlightForSearch.ts
+++ ClientApp/src/app/Models/FlightForSearch.ts
@@ -1,6 +1,6 @@
 export interface FlightForSearch{
-  departureAirportId: number;
-  arrivalAirportId: number;
-  date: string;
-  ticketsNumber: number;
+  departureAirportId?: number;
+  arrivalAirportId?: number;
+  date?: string;
+  ticketsNumber?: number;
 }
